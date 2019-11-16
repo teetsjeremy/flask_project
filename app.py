815204c0b94975aa24135682d6b37511dd87b61e
Ubuntu <ubuntu@ip-172-31-28-63.us-west-2.compute.internal>
2019-11-16 00:26:15 +0000

    updated contact

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
+app.secret_key = "some_secret"
 
 
 @app.route('/')
@@ -27,8 +28,10 @@ def about_member(member_name):
                 member = obj
     return render_template("member.html", member=member)           
     
-@app.route('/contact')
+@app.route('/contact', methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have recieved your message!".format(request.form["name"]))
     return render_template("contact.html", page_title="Contact")
 
 
